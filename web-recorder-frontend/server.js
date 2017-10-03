2747131411a28e8509fa47bce5dc9ecdbc0ba3e5
Dimitris Niras <niras.94@gmail.com>
2017-10-04 00:00:57 +0300

    Update server.js

--- web-recorder-frontend/server.js
+++ web-recorder-frontend/server.js
@@ -7,7 +7,7 @@ app.get('*', function(req, res) {
   res.sendFile(__dirname + '/dist/' + 'index.html')
 });
 
-app.listen(8080, function() {
+app.listen(80, function() {
     console.log('Frontend app listening on port 8080!');
 });
 
