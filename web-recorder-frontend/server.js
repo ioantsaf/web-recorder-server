a2ee961d5365d9ddbe92babbdf56a950f7fc2025
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2017-10-08 17:59:40 +0300

    Use 8080 as frontend port

--- web-recorder-frontend/server.js
+++ web-recorder-frontend/server.js
@@ -7,7 +7,7 @@ app.get('*', function(req, res) {
   res.sendFile(__dirname + '/dist/' + 'index.html')
 });
 
-app.listen(80, function() {
-    console.log('Frontend app listening on port 80!');
+app.listen(8080, function() {
+    console.log('Frontend app listening on port 8080!');
 });
 
