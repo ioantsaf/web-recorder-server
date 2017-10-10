4eabe1eccb8aa58403864d0b30882c4c10972c3f
Ioannis Tsafaras <ioantsaf@grid.auth.gr>
2017-10-10 19:07:01 +0300

    Use karma-junit-reporter for automated tests

--- web-recorder-frontend/karma.conf.js
+++ web-recorder-frontend/karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
@@ -31,14 +32,18 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+    // reporters: config.angularCli && config.angularCli.codeCoverage
+    //          ? ['progress', 'coverage-istanbul']
+    //          : ['progress', 'kjhtml'],
+    reporters: ['dots', 'junit'],
+    junitReporter: {
+      outputFile: 'test-results.xml'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['ChromeHeadless'],
-    singleRun: false
+    singleRun: true
   });
 };
